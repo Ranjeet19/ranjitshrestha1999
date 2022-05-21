637e648f40f6206e7cfcce41b19dcf03242c2f1a
ranjeet shrestha <53162983+Ranjeet19@users.noreply.github.com>
2022-05-21 09:29:16 +0545

    portfoli
    this is my  portfolio first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed[".typing",{
+    strings:["Web Designer", "Flutter App Developer", "Graphics Designer", "Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+}]
